48dfc00801fde9fd189e5c21ccb26791c84675dc
Paul Park <park.paul.94@gmail.com>
2018-06-06 09:26:18 -0400

    Deleted console log lines, changed page redirect to home page after registering.

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -2,24 +2,20 @@ $(function() {
 
     $('#signInBtn').on('click', (event) => {
         event.preventDefault();
-        console.log('I have been clicked')
         $.ajax('/signin', {
             type: 'GET',
             cache: false
         }).done((data) => {
-            console.log('shall have been routed to SignIn page now');
             $('body').html(data);
         }).catch((err) => console.log(err));
     });
 
     $('#signUpBtn').on('click', (event) => {
         event.preventDefault();
-        console.log('I have been clicked')
         $.ajax('/signup', {
             type: 'GET',
             cache: false
         }).done((data) => {
-            console.log('shall have been routed to SignUp page now')
             $('body').html(data);
         }).catch((err) => console.log(err));
     })
